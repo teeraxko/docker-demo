3651bd4cfa0950f9004ce5fe2d87633ca3d50a44
Teeraxko <teera_k@tripetch-it.co.th>
2023-12-12 15:38:03 +0700

    completed

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -18,7 +18,7 @@ const Search = ({ word, setWord, handleSubmit }) => {
               </Col>
               <Col>
                 <Button variant="primary" type="submit">
-                  Search
+                  Search Ple
                 </Button>
               </Col>
             </Form.Row>
